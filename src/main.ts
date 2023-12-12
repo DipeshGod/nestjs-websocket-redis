74e7899c62bcb0adaa41b9295c27d338ee496118
Dipesh Chaulagain <dipesh.chaulagain@upaya.com.np>
2023-12-12 15:24:30 +0545

    add redis adapter

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,19 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { PrismaClientExceptionFilter } from './prisma/prisma.exception.filter';
+import { RedisIoAdapter } from './adapters/redis-io.adapter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new PrismaClientExceptionFilter());
+
+  const redisIoAdapter = new RedisIoAdapter(app);
+  await redisIoAdapter.connectToRedis();
+
+  app.useWebSocketAdapter(redisIoAdapter);
+
   await app.listen(3000);
 }
 bootstrap();
--- src/room/room.gateway.ts
+++ src/room/room.gateway.ts
@@ -1,10 +1,57 @@
-import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+import {
+  ConnectedSocket,
+  MessageBody,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import { Socket, Server } from 'socket.io';
 
-@WebSocketGateway()
+@WebSocketGateway({
+  cors: {
+    origin: '*',
+  },
+  transports: ['websocket'],
+})
 export class RoomGateway {
+  @WebSocketServer() server: Server;
+
   @SubscribeMessage('helloWorld')
   handleMessage(client: any, payload: any) {
     console.log('payload', payload);
     client.emit('helloWorld', { data: 'Hello World From Server' });
+  }
+
+  @SubscribeMessage('joinTripLocationLive')
+  handleJoinTripLocationLive(
+    @MessageBody() data: { tripId: string },
+    @ConnectedSocket() client: Socket,
+  ) {
+    const { tripId } = data;
+
+    // join the client to the trip channel (room)
+    client.join(`${tripId}`);
+
+    // notify the client that they have successfully joined the channel
+    client.emit(
+      'tripChannelJoined',
+      `You have joined the trip channel for Trip ID: ${tripId}`,
+    );
+  }
+
+  @SubscribeMessage('liveLocationOfTrip')
+  handleLiveLocation(
+    @MessageBody()
+    data: {
+      longitute: number;
+      latitute: number;
+      tripId: number;
+    },
+  ) {
+    //save to dabatase
+    console.log('liverLocationCommingFromClient', data);
+
+    //notify new location
+    this.server.to(`${data.tripId}`).emit('liveLocationUpdate', data);
   }
 }
